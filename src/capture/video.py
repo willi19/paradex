40dbd2ebf0042c793040eb9b57f474ccbd5c20d7
nagooon <prom3170@gmail.com>
2025-02-27 15:29:53 +0900

    fix minor error : put name to cameramanager

--- src/capture/video.py
+++ src/capture/video.py
@@ -3,7 +3,7 @@ import os
 import argparse
 
 def main(name):
-    manager = CameraManager(num_cameras=4, duration=30, name="test", is_streaming=False)
+    manager = CameraManager(num_cameras=4, duration=30, name=name, is_streaming=False)
     manager.start()
 
 if __name__ == "__main__":

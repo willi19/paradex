ee21214ebb24de3615d507e9bf616e1dcbea7aed
willi19 <willi19@snu.ac.kr>
2025-03-16 10:58:12 +0900

    change to get list

--- src/process/undistort_select.py
+++ src/process/undistort_select.py
@@ -18,37 +18,39 @@ def process_video(args):
     
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Manage timestamped directories.")
-    parser.add_argument("--name", type=str, required=True, help="Name of the directory to split.")
+    # parser.add_argument("--name", type=str, required=True, help="Name of the directory to split.")
+    parser.add_argument("--name_list", type=str, nargs="+", required=True, help="List of directories to split.")
     parser.add_argument("--cam_param", type=str, default=None, help="Camera parameter file.")
     args = parser.parse_args()
 
     
-    name = args.name
     intrinsics, extrinsics = load_cam_param(args.cam_param)
 
-    if not os.path.exists(os.path.join(capture_path_list[0], "capture", name)) or not os.path.exists(os.path.join(capture_path_list[1], "capture", name)):
-        print(f"Directory {name} not found.")
-        exit()
-    
-    index_list = os.listdir(os.path.join(capture_path_list[0], "capture", name))
-    print(name, capture_path_list)
+    for name in args.name_list:
+        name = args.name
 
-    video_list = []
-    index_offset = 0
-    for index in index_list:
-        for capture_path in capture_path_list:
-            
-            selected_frame = json.load(open(os.path.join(shared_dir, "capture", name, index, "selected_frame.json")))
-            video_dir = os.path.join(capture_path, "capture", name, index)
-
-            for vp in get_video_list(video_dir):
-                serial = os.path.basename(vp[0]).split("_")[0]
-                video_list.append((vp, intrinsics[serial], selected_frame, index_offset))
-
-        index_offset += len(selected_frame.keys())
-
-    with Pool(processes=cpu_count()) as pool:
-        with tqdm(total=len(video_list), desc="Total Progress", unit="dir") as outer_bar:
-            for _ in pool.imap_unordered(process_video, video_list):
-                outer_bar.update(1)
+        if not os.path.exists(os.path.join(capture_path_list[0], "capture", name)) or not os.path.exists(os.path.join(capture_path_list[1], "capture", name)):
+            print(f"Directory {name} not found.")
+            continue    
         
+        index_list = os.listdir(os.path.join(capture_path_list[0], "capture", name))
+        
+        video_list = []
+        index_offset = 0
+        for index in index_list:
+            for capture_path in capture_path_list:
+                
+                selected_frame = json.load(open(os.path.join(shared_dir, "capture", name, index, "selected_frame.json")))
+                video_dir = os.path.join(capture_path, "capture", name, index)
+
+                for vp in get_video_list(video_dir):
+                    serial = os.path.basename(vp[0]).split("_")[0]
+                    video_list.append((vp, intrinsics[serial], selected_frame, index_offset))
+
+            index_offset += len(selected_frame.keys())
+
+        with Pool(processes=cpu_count()) as pool:
+            with tqdm(total=len(video_list), desc="Total Progress", unit="dir") as outer_bar:
+                for _ in pool.imap_unordered(process_video, video_list):
+                    outer_bar.update(1)
+            

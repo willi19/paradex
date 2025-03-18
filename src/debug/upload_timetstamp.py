c149699c385f867acd77c43857c0bfb1bd38df30
willi19 <willi19@snu.ac.kr>
2025-03-18 21:30:56 +0900

    minor error fix

--- src/debug/upload_timetstamp.py
+++ src/debug/upload_timetstamp.py
@@ -4,9 +4,9 @@ import argparse
 import time
 from tqdm import tqdm
 from paradex.utils.upload_file import get_total_size, copy_to_nfs
+from paradex.utils.io import capture_path_list
 
 home_dir = os.path.expanduser("~")
-capture_path_list = [os.path.join(home_dir, f"captures{i}") for i in range(1,3)]
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Recursively copy files and directories, skipping identical files but overwriting corrupt ones.")
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     destination_path = os.path.join(home_dir, "shared_data", "capture", args.name)
 
     for capture_path in capture_path_list:
-        source_path = os.path.join(capture_path, args.name)
+        source_path = os.path.join(capture_path, "capture", args.name)
         total_size = get_total_size(source_path, destination_path)
         
         copy_to_nfs(source_path, destination_path, total_size, end_with=".json")

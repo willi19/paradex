784641049d27b42b05c70ca16665568017878bb6
willi19 <willi19@snu.ac.kr>
2025-03-18 18:58:16 +0900

    debug

--- paradex/utils/upload_file.py
+++ paradex/utils/upload_file.py
@@ -65,8 +65,9 @@ def copy_to_nfs(source_path, destination_path, total_size, copied_size=0, end_wi
     try:
         if os.path.isfile(source_path):
             if end_with:
-                if not source_path[-len(end_with):] == end_with:
+                if source_path[-len(end_with):] != end_with:
                     return copied_size
+            print(source_path, source_path[-len(end_with):], end_with)
             copied_size = copy_file_with_progress(source_path, destination_path, total_size, copied_size)
         else:
             os.makedirs(destination_path, exist_ok=True)

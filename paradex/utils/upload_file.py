e2e81da8a92336f22b0c69fb143ffca5efe62961
willi19 <willi19@snu.ac.kr>
2025-03-18 21:21:43 +0900

    minor error fix

--- paradex/utils/upload_file.py
+++ paradex/utils/upload_file.py
@@ -67,7 +67,6 @@ def copy_to_nfs(source_path, destination_path, total_size, copied_size=0, end_wi
             if end_with is not None:
                 if source_path[-len(end_with):] != end_with:
                     return copied_size
-            print(source_path, destination_path)
             copied_size = copy_file_with_progress(source_path, destination_path, total_size, copied_size)
         else:
             os.makedirs(destination_path, exist_ok=True)

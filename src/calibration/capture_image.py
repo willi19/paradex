b2a2406511610a92dae819a2681d38323e9ad89a
nagooon <prom3170@gmail.com>
2025-03-23 20:09:15 +0900

    minor error fix

--- src/calibration/capture_image.py
+++ src/calibration/capture_image.py
@@ -5,6 +5,46 @@ import paradex
 from paradex.camera.camera_loader import CameraManager
 from paradex.utils.io import home_dir, calib_path_list
 
+def capture_images_from_all_cameras(save_path, lens_info_path, camera_config_path):
+    """
+    Captures a single image from all connected cameras and saves them.
+    """
+    # Iterate through interfaces and cameras
+    camera_config = json.load(open(camera_config_path, "r"))
+    lens_info = json.load(open(lens_info_path, "r"))
+
+    # Initialize the Spinnaker system
+    system = ps.System.GetInstance()
+
+    ret = {}
+    camera_list = system.GetCameras()
+    for pCam in camera_list:
+        cam = Camera(pCam, camera_config, lens_info, save_path, False)
+        for frame_num in range(1):
+            for _ in range(10):
+                pImg, retcode = cam.get_capture(200) # 200 ms timeout
+                if retcode:
+                    cvImg = spin2cv(pImg, 1536, 2048)  # Adjust resolution as needed
+                    image_save_path = os.path.join(save_path , f"{cam.serialnum}.png") 
+                    cv2.imwrite(str(image_save_path), cvImg)
+                    print(f"Image saved at: {image_save_path}")
+                    cam.stop_camera()
+                    ret[cam.serialnum] = cvImg
+                    break
+
+                else:
+                    print(f"Failed to capture image from camera {cam.serialnum}")
+
+    for pCam in camera_list:
+        del pCam
+
+    camera_list.Clear()
+
+    system.ReleaseInstance()
+
+    print("Image capture completed for all cameras.")
+    return ret
+
 def find_latest_directory(is_init):
     if is_init:
         now = datetime.datetime.now()
@@ -36,9 +76,11 @@ def main():
     for calib_path in calib_path_list:
         os.makedirs(os.path.join(calib_path, str(name), str(index), "images","00001"), exist_ok=True)
         
-    dir_name = f"calibration/{name}/{index}/images/00001"
-    manager = CameraManager(num_cameras=4, name=dir_name, is_streaming=False)
-    manager.start()
+    save_path = f"calibration/{name}/{index}/images/00001"
+    camera_config = "config/lens.json"
+    lens_info = "config/camera.json"
+    capture_images_from_all_cameras(save_path, args.num_image, lens_info, camera_config)
+
 
 if __name__ == "__main__":
     main()

fa752cedd4f0ca1916a29dba79e2ecaa4aecbf79
nagooon <prom3170@gmail.com>
2025-03-24 06:04:02 +0900

    asdf

--- src/process/undistort_select.py
+++ src/process/undistort_select.py
@@ -45,7 +45,10 @@ if __name__ == "__main__":
             print(f"Selected frame loaded for {name}/{index}.")
             for capture_path in capture_path_list:
                 
-                video_dir = os.path.join(capture_path, "capture", name, index, "video")
+                video_dir = os.path.join(capture_path, "capture", name, index, "videos")
+                if not os.path.exists(video_dir):
+                    print(f"Video directory not found for {name}/{index}.")
+                    continue
 
                 for vp in get_video_list(video_dir):
                     serial = os.path.basename(vp[0]).split("_")[0]

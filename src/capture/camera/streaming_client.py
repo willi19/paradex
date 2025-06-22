153b046cb9d544eed79680e9d345732dfb058e9c
willi19 <willi19@snu.ac.kr>
2025-06-22 17:39:57 +0900

    asdf

--- src/capture/camera/streaming_client.py
+++ src/capture/camera/streaming_client.py
@@ -21,8 +21,10 @@ while time.time() - start_time < 10:
             last_image = camera.image_array[0].copy()
 
         # image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        serial_num = camera.serial_list[0]
+        os.makedirs(f"{shared_dir}/tmp/{last_frame}", exist_ok=True)
         last_image = cv2.resize(last_image, (640, 480))
-        cv2.imwrite(f"{shared_dir}/tmp/{last_frame}.jpg", last_image)
+        cv2.imwrite(f"{shared_dir}/tmp/{last_frame}/{serial_num}.jpg", last_image)
         print(last_frame)
 camera.end()
 camera.quit()

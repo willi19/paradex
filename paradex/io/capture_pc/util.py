fe2937088068d1f7d575c93ea680692d1067daa7
willi19 <willi19@snu.ac.kr>
2025-06-24 00:44:53 +0900

    asdf

--- paradex/io/capture_pc/util.py
+++ paradex/io/capture_pc/util.py
@@ -5,7 +5,7 @@ def serialize(msg_dict):
     for key, value in msg_dict.items():
         if isinstance(value, (np.ndarray)):
             ret[key] = value.tolist()
-        elif isinstance(value, (int, float, str, bool)):
+        elif isinstance(value, (int, float, str, bool, list)):
             ret[key] = value
         else:
             raise TypeError(f"Unsupported type for key '{key}': {type(value)}")

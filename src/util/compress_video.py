dd58488db4a31d6edd5fd31d4ced4b40d878f5e1
willi19 <willi19@snu.ac.kr>
2025-07-03 19:06:23 +0900

    compress vid

--- src/util/compress_video.py
+++ src/util/compress_video.py
@@ -1,11 +1,11 @@
 import os
-from paradex.utils.io import get_video_list
+from paradex.utils.file_io import get_video_list
 from paradex.video.compress_video import compress_video
 import os
 import argparse
 from tqdm import tqdm
 from multiprocessing import Pool, cpu_count
-from paradex.utils.io import find_latest_directory, shared_dir, load_cam_param, capture_path_list, get_video_list
+from paradex.utils.file_io import find_latest_directory, shared_dir, load_cam_param, capture_path_list, get_video_list
 import json
 
 def process_video(args):
@@ -45,7 +45,7 @@ if __name__ == "__main__":
                         continue
 
                     for vp in get_video_list(video_dir):
-                        serial = os.path.basename(vp[0]).split("_")[0]
+                        serial = os.path.basename(vp[0]).split("-")[0]
                         video_list.append((vp))
 
             with Pool(processes=cpu_count()) as pool:

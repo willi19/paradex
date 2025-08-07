6e7f54f8b9856f7fa927ed891b5e3395692dfbd8
willi19 <willi19@snu.ac.kr>
2025-08-08 04:33:13 +0900

    asdf

--- src/debug/dataset_acquision/process_video.py
+++ src/debug/dataset_acquision/process_video.py
@@ -11,9 +11,6 @@ from paradex.image.undistort import undistort_img
 from paradex.image.aruco import detect_aruco
 
 def process_frame(frame, info, fid):
-    intrinsic = info
-    frame = undistort_img(frame.copy(), intrinsic)
-    
     undist_kypt, ids = detect_aruco(frame)
     data = {}
     
@@ -37,16 +34,10 @@ def process_result(video_path, data_list, frame_ids):
     np.save(os.path.join(save_path, file_name), result)
 
 def load_info(video_path):
-    save_path = get_savepath(video_path)
-    serial_num = get_serialnum(video_path)
-    
-    cam_param_path = os.path.join(shared_dir, os.path.dirname(os.path.dirname(save_path)))
-    cam_param = load_camparam(cam_param_path)[0][serial_num]
-    
-    return cam_param
+    return None
 
 start_time = time.time()
-rvp = RawVideoProcessor("debug_/aruco", process_frame=process_frame, load_info=load_info, process_result=process_result, preserve=False, overwrite=False)
+rvp = RawVideoProcessor("debug_/aruco", process_frame=process_frame, load_info=load_info, process_result=process_result, preserve=True, overwrite=False)
 while not rvp.finished():
     time.sleep(0.01)
     

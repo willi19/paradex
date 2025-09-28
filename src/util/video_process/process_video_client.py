dce34246701f95ef9878d5a037eec31fa92717f5
willi19 <willi19@snu.ac.kr>
2025-09-29 04:48:44 +0900

    erase unsidstort in video process

--- src/util/video_process/process_video_client.py
+++ src/util/video_process/process_video_client.py
@@ -12,8 +12,8 @@ from paradex.image.aruco import detect_aruco
 from paradex.io.capture_pc.raw_video_processor import RawVideoProcessorWithProgress
 
 def process_frame(frame, info, fid):
-    intrinsic = info
-    frame = undistort_img(frame.copy(), intrinsic)
+    # intrinsic = info
+    # frame = undistort_img(frame.copy(), intrinsic)
     data = {}
     print(fid)
     return frame, data
@@ -25,10 +25,10 @@ def load_info(video_path):
     save_path = get_savepath(video_path)
     serial_num = get_serialnum(video_path)
     
-    cam_param_path = os.path.join(shared_dir, os.path.dirname(os.path.dirname(save_path)))
-    cam_param = load_camparam(cam_param_path)[0][serial_num]
+    # cam_param_path = os.path.join(shared_dir, os.path.dirname(os.path.dirname(save_path)))
+    # cam_param = load_camparam(cam_param_path)[0][serial_num]
     
-    return cam_param
+    return None
 
 start_time = time.time()
 rvp = RawVideoProcessorWithProgress(process_frame=process_frame, load_info=load_info, process_result=process_result)

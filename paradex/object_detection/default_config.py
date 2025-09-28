6b10bc5b0afcfbe113d991996e149a9c821f01a5
willi19 <willi19@snu.ac.kr>
2025-09-29 02:29:07 +0900

    add _2

--- paradex/object_detection/default_config.py
+++ paradex/object_detection/default_config.py
@@ -4,16 +4,16 @@ nas_path = Path.home()/'shared_data'/'202510_demo'
 
 default_template = {
     "pringles": nas_path/"template/pringles/0",
-    "brown_ramen_von" : nas_path/"template/brown_ramen_von/0",
-    "red_ramen_von" : nas_path/"template/red_ramen_von/0",
-    "yellow_ramen_von" : nas_path/"template/yellow_ramen_von/0"
+    "brown_ramen_von_2" : nas_path/"template/brown_ramen_von_2/0",
+    "red_ramen_von_2" : nas_path/"template/red_ramen_von_2/0",
+    "yellow_ramen_von_2" : nas_path/"template/yellow_ramen_von_2/0"
 }
 
 name2prompt = {
     "pringles": "pringles",
-    "brown_ramen_von" : "brownramen",
-    "red_ramen_von" : "redramen",
-    "yellow_ramen_von" : "yellowramen",
+    "brown_ramen_von_2" : "brownramen",
+    "red_ramen_von_2" : "redramen",
+    "yellow_ramen_von_2" : "yellowramen",
 }
 
 prompt2name = {value:key for key, value in name2prompt.items()}

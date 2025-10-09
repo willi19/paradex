6017e18ce9aed76092a52613c63f6dc2e5e47c02
willi19 <willi19@snu.ac.kr>
2025-10-09 13:42:15 +0900

    fix issue

--- paradex/object_detection/multiview_utils/matcher.py
+++ paradex/object_detection/multiview_utils/matcher.py
@@ -303,7 +303,7 @@ class MatcherTo3D:
         print("after finding matches")
         if draw and len(matches)>0:
             print("Saved Img to ", image_name)
-            cv2.imwrite(image_name, make_grid_image_np(np.array(matches), 4, np.ceil(len(matches)/4).astype(int)))
+            cv2.imwrite(image_name, make_grid_image_np(np.array(matches), 4, int(len(matches)/4)+1))
 
         return src_3d_dict, tg_2d_dict, org_2d_dict
     

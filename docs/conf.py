597e0ffe3fbf3fd74aa6f40b43df62b40a5e2bc6
willi19 <willi19@snu.ac.kr>
2025-10-11 21:56:46 +0900

    fixing docs

--- docs/conf.py
+++ docs/conf.py
@@ -44,9 +44,4 @@ exclude_patterns = [
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static'] 
-
-html_theme_options = {
-    'navigation_depth': 4,
-    'collapse_navigation': False,
-    'display_version': True,
-}
+html_theme_options = {}

f50956e9abb53181a0d448ed6b4669550e27c999
willi19 <willi19@snu.ac.kr>
2025-10-30 19:56:40 +0900

    asdf

---
+++ src/validate/camera/camera_harsh.py
@@ -0,0 +1,14 @@
+from paradex.io.camera_system.camera import Camera
+from paradex.io.camera_system.pyspin import get_serial_list
+import time
+
+# Test image -> full -> image
+serial_list = get_serial_list()
+for serial_num in serial_list:
+    camera = Camera("pyspin", serial_num)
+    for _ in range(1000):
+        camera.start("image", False, "test1.png")
+        camera.stop()
+        
+    camera.end()
+    

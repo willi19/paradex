dac3fae38451b10cfb684e673d50747a6bd7cb89
willi19 <willi19@snu.ac.kr>
2025-10-30 23:30:21 +0900

    asdf

--- src/validate/camera/camera_loader_harsh.py
+++ src/validate/camera/camera_loader_harsh.py
@@ -9,7 +9,7 @@ for i in range(100):
     camera.start("image", False, "test")
     camera.stop()
 
-    camera.start("stream", False, "test",fps=30)
+    camera.start("full", False, "test",fps=30)
     time.sleep(1)
     camera.stop()
 

0fa67bc80111ec99b278b8dfc9a13348d9e485e8
willi19 <willi19@snu.ac.kr>
2025-10-30 20:34:04 +0900

    asdf

--- paradex/io/camera_system/pyspin.py
+++ paradex/io/camera_system/pyspin.py
@@ -300,7 +300,7 @@ class PyspinCamera():
         # Align to nearest multiple of THROUGHPUT_ALIGNMENT (value must be multiple of 16000)
         ValMax = ThroughputLimit.GetMax()
         ValMin = ThroughputLimit.GetMin()
-        posValMax = (int((ValMax - ValMin) / 8) // PyspinCameraConfig.THROUGHPUT_ALIGNMENT) * PyspinCameraConfig.THROUGHPUT_ALIGNMENT + ValMin
+        posValMax = (int((ValMax - ValMin)) // PyspinCameraConfig.THROUGHPUT_ALIGNMENT) * PyspinCameraConfig.THROUGHPUT_ALIGNMENT + ValMin
         # Set throughput limit 
         ThroughputLimit.SetValue(posValMax)
         

1c2daa4a5eeae1dbe4593374e56c9896d37c6a4e
willi19 <willi19@snu.ac.kr>
2025-11-07 17:09:32 +0900

    asdf

---
+++ src/capture/camera/stream_remote.py
@@ -0,0 +1,32 @@
+from threading import Event
+import time
+import argparse
+import os
+
+from paradex.io.capture_pc.camera_main import RemoteCameraController
+from paradex.io.camera_system.remote_camera_controller import remote_camera_controller
+
+from paradex.utils.keyboard_listener import listen_keyboard
+from paradex.utils.file_io import find_latest_index, shared_dir
+
+rcc = remote_camera_controller("stream_main.py")
+
+stop_event = Event()
+save_event = Event()
+
+listen_keyboard({"c":save_event, "q":stop_event})
+
+try:
+    while not stop_event.is_set():
+        
+        if not save_event.is_set():
+            time.sleep(0.01)
+            continue
+        
+        rcc.start("stream", False, fps=10)
+        tmp = input("Press Enter to stop streaming...")
+        rcc.stop()
+        save_event.clear()
+        
+finally:
+    rcc.end()

20bc67dc9d096ba1bf65f8f1fa25d28071cc3761
willi19 <willi19@snu.ac.kr>
2025-11-17 15:51:03 +0900

    asdf

--- src/validate/camera/camera_loader.py
+++ src/validate/camera/camera_loader.py
@@ -6,7 +6,7 @@ camera.load_pyspin_camera()
 
 for i in range(2):
     camera.start("full", False, "test_camloader",fps=30)
-    time.sleep(1 * 60)
+    time.sleep(1 * 2)
     camera.stop()
 
     camera.start("image", False, "test_camloader")

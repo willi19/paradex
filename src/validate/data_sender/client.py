69d7983ea38d3f0a4b88f4b2689ded1c907cffd4
willi19 <willi19@snu.ac.kr>
2025-11-18 05:11:10 +0900

    asdf

--- src/validate/data_sender/client.py
+++ src/validate/data_sender/client.py
@@ -2,7 +2,7 @@ import time
 
 from paradex.io.capture_pc.data_sender import DataPublisher
 
-dp = DataPublisher(port=5500, name="TestPublisher")
+dp = DataPublisher(name="TestPublisher")
 
 start_time = time.time()
 while True:
--- src/validate/data_sender/main.py
+++ src/validate/data_sender/main.py
@@ -3,7 +3,6 @@ import time
 from paradex.io.capture_pc.data_sender import DataCollector
 from paradex.io.capture_pc.ssh import run_script
 
-run_script("python src/validate/data_sender/client.py")
 
 dc = DataCollector()
 dc.start()

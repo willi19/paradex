9141fecac6fd277beb20160798c97655234a84ba
willi19 <willi19@snu.ac.kr>
2025-12-22 23:21:43 +0900

    asdf

--- paradex/io/camera_system/pyspin.py
+++ paradex/io/camera_system/pyspin.py
@@ -35,11 +35,10 @@ def load_camera(serialnum):
         camPtr = cam_list.GetBySerial(serialnum)
     except:
         raise ValueError(f"Camera with serial number {serialnum} not found.")
-    print(cam_info)
+    
     if serialnum in cam_info:
         gain = cam_info[serialnum]["gain"]
         exposure = cam_info[serialnum]["exposure"]
-        print(gain, exposure, "gain", "exposure", serialnum)
     else:
         gain = 3.0
         exposure = 2500.0

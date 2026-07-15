3b5e394a28c8b673eef78c929fdc7192c40abff0
gohyunsu <283095081+gohyunsu@users.noreply.github.com>
2026-07-15 14:12:20 +0900

    ci: deploy full static site

--- script/build_docs.sh
+++ script/build_docs.sh
@@ -1,11 +1,18 @@
+set -euo pipefail
+
+SPHINX_BUILD="${SPHINX_BUILD:-sphinx-build}"
+if [[ "${SPHINX_BUILD}" == "sphinx-build" && -x "${HOME}/anaconda3/envs/docs/bin/sphinx-build" ]]; then
+  SPHINX_BUILD="${HOME}/anaconda3/envs/docs/bin/sphinx-build"
+fi
+
 cd docs
 
 echo "Building documentation..."
-sphinx-build -b html . _build/html
+"${SPHINX_BUILD}" -b html . _build/html
 
 echo "Cleaning up old files..."
 rm -f *.html
-rm -rf _sources _modules
+rm -rf _sources _modules autoapi
 # _static은 지우지 않음!
 
 echo "Copying files to docs root..."
